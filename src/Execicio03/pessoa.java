42c82c2b4a731ce4212d07b06115e4f68c21058e
104940 <104940@SALA407.proway.treina>
2018-05-11 11:53:17 -0300

    MODIFICADO AS 11:53

--- src/Execicio03/pessoa.java
+++ src/Execicio03/pessoa.java
@@ -15,103 +15,91 @@ public class pessoa {
     String nomeIdadeMaior = "";
     double peso;
     double pesoMaior = 0;
-    int quantidade = 0;
+    double quantidade = 0;
     double altura = 0;
     double alturaMaior = 0;
     double nomeMaiorAltura = 0;
     double media = 0;
-    double mediaAltura = 0;
-  
+    double mediaAltura =0;
     
-    
-    //perguntar nome
+       
+    //Laço
     public void laco() {
-        nome = JOptionPane.showInputDialog("Qual o seu nome?");
         
         do{
         	obterDados();
         	 nomeIdadeMaior();
         	 nomePesoMaior();
         	 nomeAlturaMaior();
-        	 mediaAltura();
+        	 Obtermedia();
         	 
-        	
+       
+         
         
     
-        } while(!nome.equals("SAIR"));{
-        quantidade++;
-        }
-        
-        
+
+} while(!nome.equals("SAIR"));{
+    quantidade++;
+}
     }
+      //Obter Dados
+        public void obterDados () {
         
+        		//variavel para validar dados
+        		boolean valida;
+        		//perguntar nome 
+                nome = JOptionPane.showInputDialog("Informe seu nome ou SAIR para finalizar");
+        	} 		
+        		
+        		//Obter idade 
+        		public void nomeIdadeMaior() {
+                idade = Integer.parseInt(JOptionPane.showInputDialog("Informe a idade." ));
+                     if(idade > idadeMaior){
+                     idadeMaior = idade;
+                     nomeIdadeMaior = nome;
+                     }
+                     }
+               //Obter peso
+                     public void nomePesoMaior() {
+                peso = Double.parseDouble(JOptionPane.showInputDialog("Informe o seu peso."));
+                     if(peso > pesoMaior){
+                     pesoMaior = peso; 
+                     nomePesoMaior = nome;
+                     }
+                     }
+                   //Obter maior altura
+                     public void  nomeAlturaMaior() { 
+                   altura = Double.parseDouble(JOptionPane.showInputDialog("Informe a sua altura."));
+                   if(altura > alturaMaior){
+                	   alturaMaior = altura;
+                	    nomeMaior = nome;
+                   }
+                     }  
+                	  //Obter média das alturas
+                        public void Obtermedia(){
+                    double media = (altura + mediaAltura)/quantidade;
+
+                        
+
+                	
+                 //perguntar nome 
+                   nome = JOptionPane.showInputDialog("Informe seu nome ou SAIR para finalizar");
         
-    
-    	
-    		//Obter Dados
-    public void obterDados () {
-    
-    		//variavel para validar dados
-    		boolean valida;
-    	} 		
-    		
-    		//Obter idade 
-    		public void nomeIdadeMaior() {
-            idade = Integer.parseInt(JOptionPane.showInputDialog("Informe a idade." ));
-                 if(idade > idadeMaior){
-                 idadeMaior = idade;
-                 nomeIdadeMaior = nome;
-                 }
-                 }
-           //Obter peso
-                 public void nomePesoMaior() {
-            peso = Double.parseDouble(JOptionPane.showInputDialog("Informe o seu peso."));
-                 if(peso > pesoMaior){
-                 pesoMaior = peso; 
-                 nomePesoMaior = nome;
-                 }
-                 }
-               //Obter maior altura
-                 public void  nomeAlturaMaior() { 
-               altura = Double.parseDouble(JOptionPane.showInputDialog("Informe a sua altura."));
-               if(altura > alturaMaior){
-            	   alturaMaior = altura;
-            	    nomeMaiorAltura = altura;
-               }
-               
-                 
-                 }
-               
-               //Obter média das alturas
-                 public void mediaAltura() {
-                 if(media > mediaAltura){
-                	 mediaAltura = media;
-                 
-                  
-                 }
-               
-                               
-            //perguntar nome 
-            nome = JOptionPane.showInputDialog("Informe seu nome ou SAIR para finalizar");
-               
-    	
-    	
-        //Mostrar nome
-        JOptionPane.showMessageDialog(null,"O Nome do jogador maios alto "+nomeMaior+" e  "+alturaMaior+" é de");
-        JOptionPane.showMessageDialog(null,"O nome do jogador mais velho "+nomeIdadeMaior+" e  "+idadeMaior+" é de");
-        JOptionPane.showMessageDialog(null,"O nome do jogador mais pesado "+nomePesoMaior+" e   "+pesoMaior+" é de");
-    
+            
+            JOptionPane.showMessageDialog(null,"O Nome do jogador maios alto é: "+nomeMaior+" e sua altura é de "+alturaMaior+" ");
+        JOptionPane.showMessageDialog(null,"O nome do jogador mais velho é: "+nomeIdadeMaior+" e sua idade é de  "+idadeMaior+" ");
+        JOptionPane.showMessageDialog(null,"O nome do jogador mais pesado é: "+nomePesoMaior+" e sua peso é de   "+pesoMaior+" ");
+        
 
     }
         
-
-    
-                 
 }
 
 
-  
+            
+    
 
+                 
 
 
 
